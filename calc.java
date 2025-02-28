d471e9f15fedc3defeaef2425a6ec4372b43244f
Dhina <Dhina123@gmail.com>
2025-02-28 11:29:24 +0530

    added mul() div ()

--- calc.java
+++ calc.java
@@ -1,1 +1,30 @@
- start code using functions
+[200~import java.util.Scanner;
+
+public class MathOperations {
+	    public static void main(String[] args) {
+		            Scanner scanner = new Scanner(System.in);
+
+			            // Taking input from the user
+				    //         System.out.print("Enter first number: ");
+				    //                 double num1 = scanner.nextDouble();
+				    //                         
+				    //                                 System.out.print("Enter second number: ");
+				    //                                         double num2 = scanner.nextDouble();
+				    //
+				    //                                                 // Performing multiplication and division
+				    //                                                         double multiplication = num1 * num2;
+				    //                                                                 double division = num2 != 0 ? num1 / num2 : Double.POSITIVE_INFINITY; // Avoid division by zero
+				    //
+				    //                                                                         // Displaying results
+				    //                                                                                 System.out.println("Multiplication result: " + multiplication);
+				    //                                                                                         if (num2 != 0) {
+				    //                                                                                                     System.out.println("Division result: " + division);
+				    //                                                                                                             } else {
+				    //                                                                                                                         System.out.println("Division by zero is not allowed.");
+				    //                                                                                                                                 }
+				    //
+				    //                                                                                                                                         scanner.close();
+				    //                                                                                                                                             }
+				    //                                                                                                                                             }
+				    //
+
